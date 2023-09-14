bb313b1e2f53b277f5864ecd4454b15440b2fe5d
Elias <n01435671@humber.ca>
2023-09-14 10:22:49 -0400

    Second commit

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-File Operations reads a file and returns an error if the process doesn't work, or returns the file contents if the process works.
+File Operations has 3 operations it can execute: 
+
+1. Reads a file and returns an error if the process doesn't work, or returns the file contents if the process works.
+
+2. Writes specified content to a file, it will create a file if the file does not already exist,
+or replace the file content with the new content if the file does exist.
+
+3. Deletes a specified file or returns an error if the file does not exist.
--- fileOperations.js
+++ fileOperations.js
@@ -8,3 +8,25 @@ fs.readFile('example.txt', 'utf8', (err, data) => {
     }
     console.log('File content:', data);
 });
+
+// Write a file
+const FileWrite = (filename, content) => {
+    fs.writeFile(filename, content, 'utf8', (err) => {
+        if (err) {
+            console.error('Error writing to the file:', err);
+            return;
+        }
+        console.log('Content successfully written to ${filename}.');
+    });
+}
+
+//Delete a file
+const deleteFile = (filename) => {
+    fs.unlink(filename, (err) => {
+        if (err) {
+            console.error('Error deleting the file;', err);
+            return;
+        }
+        console.log('Successfully deleted ${filename}.');
+    });
+}
