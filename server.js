1c040ea91d6e17a2fd9fd66030cd74845d900e10
Elias <n01435671@humber.ca>
2023-11-30 09:39:16 -0500

    commit 9

--- server.js
+++ server.js
@@ -1,119 +1,63 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const { body, validationResult } = require('express-validator');
-
+const bcrypt = require('bcryptjs');
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const session = require('express-session');
+ 
 const app = express();
 app.use(express.json());
-
+ 
 mongoose.connect('mongodb://localhost:27017/fitnessTracker', { useNewUrlParser: true, useUnifiedTopology: true });
-
-const workoutSchema = new mongoose.Schema({
-  type: { type: String, required: true },
-  duration: { type: Number, required: true },
-  calories_burned: { type: Number, required: true }
+ 
+// 1. Create User Schema
+const userSchema = new mongoose.Schema({
+  username: String,
+  email: String,
+  password: String
 });
+ 
+const User = mongoose.model('User', userSchema);
 
-const nutritionSchema = new mongoose.Schema({
-  meal: { type: String, required: true },
-  calories: { type: Number, required: true },
-  protein: { type: Number, required: true }
-});
+// 4. Configure Passport.js
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
-const goalSchema = new mongoose.Schema({
-  goal_type: { type: String, required: true },
-  target: { type: Number, required: true }
-});
-
-const Workout = mongoose.model('Workout', workoutSchema);
-const Nutrition = mongoose.model('Nutrition', nutritionSchema);
-const Goal = mongoose.model('Goal', goalSchema);
-
-app.get('/workouts', async (req, res) => {
+// 5. Implement User Sessions
+app.use(session({
+  secret: "FitnessTrackerSecret",
+  resave: false,
+  saveUninitialized: false
+}));
+ 
+// 2, 3. Implement User Authentication Routes
+app.post('/register', async (req, res) => {
   try {
-    const workouts = await Workout.find();
-    res.json(workouts);
+    const hashedPassword = await bcrypt.hash(req.body.password, 10);
+    const newUser = new User({ username: req.body.username, email: req.body.email, password: hashedPassword });
+    await newUser.save();
+    res.json({ message: 'User registered successfully' });
   } catch (error) {
-    res.status(500).json({ message: error.message });
+    console.error(error);
+    res.status(500).json({ error: 'Internal Server Error' });
   }
 });
 
-app.post('/workouts', [
-  body('type').isLength({ min: 1 }),
-  body('duration').isNumeric(),
-  body('calories_burned').isNumeric()
-], async (req, res) => {
-  const errors = validationResult(req);
-  if (!errors.isEmpty()) {
-    return res.status(400).json({ errors: errors.array() });
-  }
-
-  try {
-    const newWorkout = new Workout(req.body);
-    await newWorkout.save();
-    res.json(newWorkout);
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
+app.post('/login', passport.authenticate('local'), (req, res) => {
+  res.json({ message: 'Logged in successfully' });
 });
 
-app.get('/nutrition', async (req, res) => {
-  try {
-    const nutritionFacts = await Nutrition.find();
-    res.json(nutritionFacts);
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
+app.get('/logout', (req, res) => {
+  req.logout();
+  res.json({ message: 'Logged out successfully' });
 });
 
-app.post('/nutrition', [
-  body('meal').isLength({ min: 1 }),
-  body('calories').isNumeric(),
-  body('protein').isNumeric()
-], async (req, res) => {
-  const errors = validationResult(req);
-  if (!errors.isEmpty()) {
-    return res.status(400).json({ errors: errors.array() });
-  }
 
-  try {
-    const newNutrition = new Nutrition(req.body);
-    await newNutrition.save();
-    res.json(newNutrition);
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
+app.listen(3000, () => {
+  console.log('Server running on <http://localhost:3000/>');
 });
 
-app.get('/goals', async (req, res) => {
-  try {
-    const goals = await Goal.find();
-    res.json(goals);
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-});
 
-app.post('/goals', [
-  body('goal_type').isLength({ min: 1 }),
-  body('target').isNumeric()
-], async (req, res) => {
-  const errors = validationResult(req);
-  if (!errors.isEmpty()) {
-    return res.status(400).json({ errors: errors.array() });
-  }
-
-  try {
-    const newGoal = new Goal(req.body);
-    await newGoal.save();
-    res.json(newGoal);
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-});
-
-const port = 3000;
-app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`);
-});
 
 
