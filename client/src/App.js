920a79d8fba78e24166583422c94bf0fd9eab255
Elias <n01435671@humber.ca>
2023-12-19 16:17:21 -0500

    commit 6

--- client/src/App.js
+++ client/src/App.js
@@ -1,66 +1,18 @@
-import React, { useState, useEffect } from "react";
-
-const App = () => {
-  const [exercises, setExercises] = useState([]);
-  const [newExercise, setNewExercise] = useState("");
-
-  // Helper function to fetch exercises
-  const fetchExercises = async () => {
-    try {
-      const response = await fetch("/api/exercises");
-      if (!response.ok) {
-        throw new Error(`HTTP error! status: ${response.status}`);
-      }
-      const data = await response.json();
-      setExercises(data);
-    } catch (error) {
-      console.error("Error fetching data:", error);
-    }
-  };
-
-  // Fetch exercises from API on component mount
-  useEffect(() => {
-    fetchExercises();
-  }, []);
-
-  // Event handler for refreshing exercises
-  const handleRefresh = () => {
-    fetchExercises();
-  };
-
-  // Event handler for adding an exercise
-  const handleAddExercise = () => {
-    if (newExercise.trim() === "") return; // Ensure the exercise is not empty
-    const exercise = {
-      id: exercises.length + 1, // simplistic ID generation
-      name: newExercise
-    };
-    setExercises([...exercises, exercise]);
-    setNewExercise("");
-  };
-
+import React from 'react';
+import { useStepTracker } from './useStepTracker';
+ 
+function StepTracker() {
+  const { steps, incrementSteps } = useStepTracker(0);
+ 
   return (
     <div>
-      <h1>Exercise List</h1>
-      <button onClick={handleRefresh}>Refresh</button>
-      <input
-        value={newExercise}
-        onChange={(e) => setNewExercise(e.target.value)}
-        placeholder="Add new exercise"
-      />
-      <button onClick={handleAddExercise}>Add Exercise</button>
-      {exercises.length > 0 ? (
-        <ul>
-          {exercises.map((exercise) => (
-            <li key={exercise.id}>{exercise.name}</li>
-          ))}
-        </ul>
-      ) : (
-        <p>No exercises available. Add some!</p>
-      )}
+      <h1>Fitness Tracker</h1>
+      <p>Steps Today: {steps}</p>
+      <button onClick={incrementSteps}>Add Step</button>
+      {steps >= 10 && <p>You've reached your step goal!</p>}
     </div>
   );
-};
-
-export default App;
+}
+ 
+export default StepTracker;
 
